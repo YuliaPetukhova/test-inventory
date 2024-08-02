a8a3396d70fb0ab887d3c6c015e9af4583adaf36
Petukhova Yulia <petukhova.yulia99@gmail.com>
2024-08-02 15:48:27 +0500

    появление модального окна

---
+++ src/components/inventory-page/panels/ModalPanel.vue
@@ -0,0 +1,173 @@
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+
+@Options({
+  components: {}
+})
+export default class ModalPanel extends Vue {
+}
+</script>
+
+<template>
+  <div class="panel">
+
+    <div class="panel-image">
+
+      <div class="cell-image-container">
+        <div class="cell-image-blur"></div>
+        <div class="cell-image-main"></div>
+      </div>
+
+    </div>
+
+    <div class="modal-panel-content">
+      <p class="ghost-element">Lorem</p>
+      <p class="ghost-element">Lorem</p>
+      <p class="ghost-element">Lorem</p>
+      <p class="ghost-element">Lorem</p>
+      <p class="ghost-element">Lorem</p>
+    </div>
+
+    <div>
+      <button type="button" class="modal-button-delete">Удалить предмет</button>
+    </div>
+
+    <div class="modal-form-number">
+      <input type="number" min="0" class="modal-number" placeholder="Введите количество">
+
+      <div class="modal-buttons">
+        <button type="button" class="modal-button-cancel">Отмена</button>
+        <button type="button" class="button-confirm">Подтвердить</button>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+.panel {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  position: relative;
+}
+
+.panel-image {
+  padding: 55px 60px 30px 60px;
+  box-sizing: border-box;
+
+  .cell-image-container {
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+    justify-content: center;
+    position: relative;
+    height: 130px;
+
+    .cell-image-main {
+      background-color: #7FAA65;
+      width: 115px;
+      height: 115px;
+    }
+
+    .cell-image-blur {
+      position: absolute;
+      margin: -19px 0px 29px 32px;
+      width: 115px;
+      height: 115px;
+      background-color: rgba(127, 170, 101, 0.35);
+      backdrop-filter: blur(22px);
+    }
+  }
+}
+
+.modal-panel-content {
+  margin: 0 22px 0 22px;
+}
+
+.modal-panel-content p {
+  padding: 0;
+  margin: 0 0 14px 0;
+}
+
+.modal-button-delete {
+  background-color: #FA7272;
+  width: 220px;
+  height: 39px;
+  border-radius: 8px;
+  color: white;
+  font-size: 14px;
+  border: 1px solid #FA7272;
+  margin: 0 15px 5px 15px;
+  cursor: pointer;
+  padding: 0;
+}
+
+.modal-number {
+  width: 210px;
+  height: 40px;
+  color: white;
+  background-color: #262626;
+  border: 1px solid #4D4D4D;
+  border-radius: 4px;
+  margin: 0 21px 5px 21px;
+  box-sizing: border-box;
+  padding: 0 12px;
+  outline: none;
+}
+
+.modal-buttons {
+  display: flex;
+  flex-direction: row;
+  padding: 0 21px;
+  box-sizing: border-box;
+}
+
+.modal-button-cancel {
+  width: 88px;
+  height: 33px;
+  background-color: white;
+  color: #2D2D2D;
+  border-radius: 8px;
+  font-size: 14px;
+  border: 1px solid #2D2D2D;
+  margin-right: 10px;
+  cursor: pointer;
+}
+
+.modal-button-cancel:hover {
+  box-shadow: 0 0 17px rgba(250, 114, 114, 0.7);
+}
+
+.button-confirm {
+  width: 112px;
+  height: 33px;
+  background-color: #FA7272;
+  color: #ffffff;
+  border-radius: 8px;
+  font-size: 14px;
+  border: 1px solid #FA7272;
+  cursor: pointer;
+}
+
+.button-confirm:hover {
+  box-shadow: 0 0 17px rgba(250, 114, 114, 0.7);
+}
+
+
+.ghost-element {
+  width: auto;
+  background: linear-gradient(to right, #3C3C3C, #444444, #333333);
+  animation: loading 1.5s infinite alternate; /* Анимация */
+  border-radius: 12px;
+}
+
+@keyframes loading {
+  0% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
--- src/components/inventory-page/panels/RightPanel.vue
+++ src/components/inventory-page/panels/RightPanel.vue
@@ -1,14 +1,36 @@
 <script lang="ts">
 import {Options, Vue} from 'vue-class-component';
+import ModalPanel from "@/components/inventory-page/panels/ModalPanel.vue";
+import {Icon} from "@iconify/vue";
 
-@Options({})
+@Options({
+  components: {Icon, ModalPanel}
+})
 export default class RightPanel extends Vue {
+  showModalPanel() {
+    const showModalPanel = document.querySelector('.modal-panel');
+    showModalPanel!.classList.add('show');
+  }
 
+  closeModal() {
+    const closeModalPanel = document.querySelector('.modal-panel');
+    closeModalPanel!.classList.remove('show');
+  }
 }
 </script>
 
 <template>
   <div class="right-panel">
+    <div class="table-cell" @click="showModalPanel">
+
+      <div class="cell-image-container">
+        <div class="cell-image-blur"></div>
+        <div class="cell-image-main"></div>
+      </div>
+
+      <div class="counter">7</div>
+    </div>
+
     <div class="table-cell">
 
       <div class="cell-image-container">
@@ -16,7 +38,27 @@ export default class RightPanel extends Vue {
         <div class="cell-image-main"></div>
       </div>
 
-      <div class="counter">77</div>
+      <div class="counter">4</div>
+    </div>
+
+    <div class="table-cell">
+
+      <div class="cell-image-container">
+        <div class="cell-image-blur"></div>
+        <div class="cell-image-main"></div>
+      </div>
+
+      <div class="counter">3</div>
+    </div>
+
+    <div class="table-cell">
+
+      <div class="cell-image-container">
+        <div class="cell-image-blur"></div>
+        <div class="cell-image-main"></div>
+      </div>
+
+      <div class="counter">9</div>
     </div>
 
     <div class="table-cell">
@@ -89,67 +131,43 @@ export default class RightPanel extends Vue {
       <div class="counter"></div>
     </div>
 
-    <div class="table-cell">
+    <div class="table-cell"></div>
 
-      <div class="cell-image-container">
-        <div class="cell-image-blur"></div>
-        <div class="cell-image-main"></div>
-      </div>
-
-      <div class="counter"></div>
-    </div>
-
-    <div class="table-cell">
-
-      <div class="cell-image-container">
-        <div class="cell-image-blur"></div>
-        <div class="cell-image-main"></div>
-      </div>
-
-      <div class="counter"></div>
-    </div>
-
-    <div class="table-cell">
-
-      <div class="cell-image-container">
-        <div class="cell-image-blur"></div>
-        <div class="cell-image-main"></div>
-      </div>
-
-      <div class="counter"></div>
+    <div class="modal-panel">
+      <Icon icon="carbon:close" class="close-panel-icon" @click="closeModal"/>
+      <modal-panel></modal-panel>
     </div>
   </div>
+
 
 </template>
 
 <style scoped lang="scss">
-
 .right-panel {
   display: flex;
   flex-wrap: wrap;
   flex-direction: row;
   align-content: flex-start;
   align-items: center;
-  width: 100%;
+  width: 525px;
   height: 500px;
   background-color: #262626;
   border: 1px solid #4D4D4D;
   border-radius: 12px;
+  position: relative;
 }
 
 .table-cell {
-  flex: 1;
+  position: relative;
   height: 100px;
+  width: 20%;
   border-bottom: 1px solid #4D4D4D;
   border-right: 1px solid #4D4D4D;
   cursor: pointer;
-
-  &:last-child {
-    border-top-right-radius: 12px
-  }
+  box-sizing: border-box;
 
   &:first-child {
-    border-top-left-radius: 12px
+    border-top-left-radius: 12px;
   }
 
   .cell-image-container {
@@ -168,13 +186,43 @@ export default class RightPanel extends Vue {
 
     .cell-image-blur {
       position: absolute;
-      margin: 23px 25px 29px 32px;
+      margin: 23px 19px 29px 32px;
       width: 54px;
       height: 54px;
       background-color: rgba(127, 170, 101, 0.35);
       backdrop-filter: blur(22px);
     }
   }
+
+  .counter {
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    right: 0;
+    bottom: 0;
+    border-radius: 5px 0 0 0;
+    padding: 2px 1px 2px 4px;
+    border: 1px solid #4D4D4D;
+    border-bottom: none;
+    font-size: 10px;
+    text-align: center;
+    line-height: 1;
+    color: #ffffff;
+    box-sizing: border-box;
+  }
+}
+
+.table-cell:nth-child(5n+5) {
+  border-right: none;
+}
+
+.table-cell:nth-child(5n+5):hover {
+  background-color: #2F2F2F;
+  border-top-right-radius: 12px;
+}
+
+.table-cell:nth-child(1n+1) {
+  border-left: none;
 }
 
 .table-cell:hover {
@@ -184,7 +232,7 @@ export default class RightPanel extends Vue {
 .ghost-element {
   width: auto;
   background: linear-gradient(to right, #3C3C3C, #444444, #333333);
-  animation: loading 1.5s infinite alternate; /* Анимация */
+  animation: loading 1.5s infinite alternate;
   border-radius: 12px;
 }
 
@@ -195,5 +243,34 @@ export default class RightPanel extends Vue {
   100% {
     opacity: 1;
   }
+}
+
+.modal-panel {
+  height: 500px;
+  background-color: rgba(38, 38, 38, 0.96);
+  border: 1px solid #4D4D4D;
+  border-radius: 12px;
+  transition: width 0.6s ease, opacity 0.1s ease;
+  box-sizing: border-box;
+  opacity: 0;
+  width: 0;
+  top: 0;
+  position: absolute;
+  right: 0;
+}
+
+.modal-panel.show {
+  opacity: 1;
+  width: 250px;
+}
+
+.close-panel-icon {
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  top: 8px;
+  right: 8px;
+  cursor: pointer;
+  z-index: 1;
 }
 </style>
